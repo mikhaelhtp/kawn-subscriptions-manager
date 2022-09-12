c337c53f0558560e865b12d46de840e2d5799f5f
Joash Xu <joash.xu@gmail.com>
2022-09-12 13:59:29 +0700

    Initial commit.

---
+++ kawn_subscriptions_manager/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from kawn_subscriptions_manager.users.models import User
+from kawn_subscriptions_manager.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ kawn_subscriptions_manager/users/tests/test_models.py
@@ -0,0 +1,5 @@
+from kawn_subscriptions_manager.users.models import User
+
+
+def test_user_get_absolute_url(user: User):
+    assert user.get_absolute_url() == f"/users/{user.username}/"
