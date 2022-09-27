5a5b744c1289c61e04b0d8e2518236f3b83c89dd
Ivanowsky Habeahan <ivanowskyfernandez@gmail.com>
2022-09-27 10:09:53 +0700

    Update client models.py
    Make column is_active in client models.py

--- kawn_subscriptions_manager/clients/models.py
+++ kawn_subscriptions_manager/clients/models.py
@@ -8,5 +8,6 @@ class Client(models.Model):
     name = models.CharField(_("Name"), max_length=255)
     business_name = models.CharField(_("Business Name"), max_length=255)
     user = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
