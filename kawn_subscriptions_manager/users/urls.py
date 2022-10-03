003c212f07fc8135d83d84427fa9044edc4fd787
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-10-03 09:43:22 +0700

    Update users urls

--- kawn_subscriptions_manager/users/urls.py
+++ kawn_subscriptions_manager/users/urls.py
@@ -1,29 +1,24 @@
 from django.urls import path
-from django.contrib import admin
-from . import views
-
-from django.views.generic import TemplateView
-from kawn_subscriptions_manager.users.views import (
-    user_detail_view,
-    user_redirect_view,
-    user_update_view,
-    add_users,
-    list_users,
-    edit_users,
-    delete_users,
-    # invite_users,
+from .views import (
+    UserUpdateView,
+    UserDetailView,
+    UserRedirectView,
+    AddUsers,
+    EditUsers,
+    DeleteUsers,
+    ListUsers,
     profile,
 )
 
 app_name = "users"
 urlpatterns = [
-    # path("invite_users/", views.invite, name="inviteUsers"),
+    # path("invite_users/", invite, name="inviteUsers"),
     path("profile/", profile, name="profile_users"),
-    path("add_users/", view=add_users, name="add_users"),
-    path("list_users/", view=list_users, name="list_users"),
-    path("edit_users/<int:pk>", view=edit_users, name="edit_users"),
-    path("delete_users/<int:pk>", view=delete_users, name="delete_users"),
-    path("redirect/", view=user_redirect_view, name="redirect"),
-    path("update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
+    path("list/", ListUsers.as_view(), name="list_users"),
+    path("add/", AddUsers.as_view(), name="add_users"),
+    path("edit/<int:pk>", EditUsers.as_view(), name="edit_users"),
+    path("delete/<int:pk>", DeleteUsers.as_view(), name="delete_users"),
+    path("redirect/", UserRedirectView.as_view(), name="redirect"),
+    path("update/", UserUpdateView.as_view(), name="update"),
+    path("<str:username>/", UserDetailView.as_view(), name="detail"),
 ]
