7265e55d95d63be3efdace91b6d21fe1655cd73c
Mikhael Hutapea <mhutapea751@gmail.com>
2022-10-05 09:33:29 +0700

    Add test client file
    The test file consists of __init__.py and test_urls.py

---
+++ kawn_subscriptions_manager/clients/tests/test_urls.py
@@ -0,0 +1,42 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from kawn_subscriptions_manager.clients.views import (
+    ListClient,
+    AddClient,
+    UpdateClient,
+    DeleteClient,
+    deactivate_client,
+    ListOutlet, 
+    AddOutlet,
+)
+
+
+class TestUrls(SimpleTestCase):
+    def test_list_url_resolves(self):
+        url = reverse("clients:list_client")
+        # print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, ListClient)
+
+    def test_add_url_resolves(self):
+        url = reverse("clients:add_client")
+        self.assertEquals(resolve(url).func.view_class, AddClient)
+
+    def test_update_url_resolves(self):
+        url = reverse("clients:update_client", args=(1,))
+        self.assertEquals(resolve(url).func.view_class, UpdateClient)
+
+    def test_delete_url_resolves(self):
+        url = reverse("clients:delete_client", args=(1,))
+        self.assertEquals(resolve(url).func.view_class, DeleteClient)
+
+    def test_deactivate_url_resolves(self):
+        url = reverse("clients:deactivate_client", args=(1,))
+        self.assertEquals(resolve(url).func, deactivate_client)
+
+    def test_list_outlet_url_resolves(self):
+        url = reverse("clients:list_outlet_client", args=(1,))
+        self.assertEquals(resolve(url).func.view_class, ListOutlet)
+
+    def test_add_outlet_url_resolves(self):
+        url = reverse("clients:add_outlet_client", args=(1,))
+        self.assertEquals(resolve(url).func.view_class, AddOutlet)
