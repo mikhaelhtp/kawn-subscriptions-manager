8fede25f9ccf5cdc0a8713bf6e231f1e810781df
Mikhael Hutapea <mhutapea751@gmail.com>
2022-10-26 09:40:27 +0700

    Update forms.py

--- kawn_subscriptions_manager/subscriptions/forms.py
+++ kawn_subscriptions_manager/subscriptions/forms.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 from django import forms
 from django.forms import ModelForm
 
-from kawn_subscriptions_manager.clients.models import Outlet
+from kawn_subscriptions_manager.clients.models import Account, Outlet
 from .models import SubscriptionPlan,Subscription
 
 
@@ -16,16 +16,18 @@ class AddSubscriptionForm(ModelForm):
             "expires": forms.DateTimeInput({"type": "date"}),
         }
 
-    # def __init__(self, user, *args, **kwargs):
-    #     super(AddSubscriptionForm, self).__init__(*args, **kwargs)
-    #     subscription = Subscription.objects.values_list('outlet_id')
-    #     outlet = Outlet.objects.exclude(id__in = subscription).filter(user_id = user.id)
-    #     # outlet = Outlet.objects.filter(client_id__in=client)
-    #     outlets = [(i.id, i.name) for i in outlet]
+    def __init__(self, user, *args, **kwargs):
+        super(AddSubscriptionForm, self).__init__(*args, **kwargs)
+        account = Account.objects.values_list("id").filter(user_id = user.id)
+        subscription = Subscription.objects.values_list('outlet_id')
+        #harus dihubungkan ke account
+        outlet = Outlet.objects.exclude(id__in = subscription).filter(account_id__in = account)
+        # outlet = Outlet.objects.filter(client_id__in=client)
+        outlets = [(i.id, i.name) for i in outlet]
 
-    #     subscriptionplan = SubscriptionPlan.objects.filter(is_active=True)
-    #     subscriptionplans = [(i.id, i.name) for i in subscriptionplan]
+        subscriptionplan = SubscriptionPlan.objects.filter(is_active=True)
+        subscriptionplans = [(i.id, i.name) for i in subscriptionplan]
 
-    #     self.fields["outlet"].choices = outlets
-    #     self.fields["subscriptionplan"].choices = subscriptionplans
-    #     self.fields["subscriptionplan"].label = "Subscription Plan"
+        self.fields["outlet"].choices = outlets
+        self.fields["subscriptionplan"].choices = subscriptionplans
+        self.fields["subscriptionplan"].label = "Subscription Plan"
