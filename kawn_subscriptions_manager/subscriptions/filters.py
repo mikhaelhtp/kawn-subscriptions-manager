1ebb052061674419030e639d2dcea78568eb674b
Mikhael Hutapea <mhutapea751@gmail.com>
2022-10-26 16:05:10 +0700

    Move filters.py
    Move filters.py to subscriptions folder

--- kawn_subscriptions_manager/subscriptions/filters.py
+++ kawn_subscriptions_manager/subscriptions/filters.py
@@ -1,19 +1,22 @@
 from django_filters import DateFilter, ChoiceFilter
 import django_filters
 
-from kawn_subscriptions_manager.subscriptions.models import (
+from .models import (
     SubscriptionPlan,
     Subscription,
 )
 
 
 class SubscriptionPlanFilter(django_filters.FilterSet):
+    # model = SubscriptionPlan
     STATUS_CHOICES = (
         (0, "Nonactive"),
         (1, "Active"),
     )
 
-    is_active = ChoiceFilter(choices=STATUS_CHOICES, label="Status")
+    is_active = ChoiceFilter(choices=STATUS_CHOICES, label="Status:", empty_label="All")
+    # trial_unit = ChoiceFilter(label = "Trial Unit", lookup_expr = "icontains", empty_label="All")
+    # recurrence_unit = ChoiceFilter(label = "Recurrence Unit", lookup_expr = "icontains", empty_label="All")
 
     class Meta:
         model = SubscriptionPlan
@@ -21,9 +24,9 @@ class SubscriptionPlanFilter(django_filters.FilterSet):
 
     def __init__(self, *args, **kwargs):
         super(SubscriptionPlanFilter, self).__init__(*args, **kwargs)
-        self.filters["trial_unit"].label = "Trial Unit"
+        self.filters["trial_unit"].label = "Trial Unit:"
         self.filters["trial_unit"].lookup_expr = "icontains"
-        self.filters["recurrence_unit"].label = "Recurrence Unit"
+        self.filters["recurrence_unit"].label = "Recurrence Unit:"
         self.filters["recurrence_unit"].lookup_expr = "icontains"
 
 
@@ -33,9 +36,13 @@ class SubscriptionFilter(django_filters.FilterSet):
         (1, "Active"),
     )
 
-    active = ChoiceFilter(choices=STATUS_CHOICES, label="Status")
-    subscriptionplan = ChoiceFilter(label='Subscription Plan', empty_label="All", choices=[])
-    expires = DateFilter(field_name="expires", lookup_expr="lte", label="Expired Date")
+    active = ChoiceFilter(choices=STATUS_CHOICES, label="Status:", empty_label="All")
+    subscriptionplan = ChoiceFilter(
+        label="Subscription Plan:", empty_label="All", choices=[]
+    )
+    expires = DateFilter(
+        field_name="expires", lookup_expr="lte", label="Exp Date Less Than:"
+    )
 
     class Meta:
         model = Subscription
@@ -45,4 +52,4 @@ class SubscriptionFilter(django_filters.FilterSet):
         super(SubscriptionFilter, self).__init__(*args, **kwargs)
         subscription_plan = SubscriptionPlan.objects.all()
         subscription_plans = [(i.id, i.name) for i in subscription_plan]
-        self.filters["subscriptionplan"].extra['choices'] = subscription_plans
+        self.filters["subscriptionplan"].extra["choices"] = subscription_plans
