1a9faa7a615b263a1ad6b1c3b00e2f6a53d964ba
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-11-01 17:20:24 +0700

    Add new static files
    Add datatables, add jquery, add sweetalert2

---
+++ kawn_subscriptions_manager/static/js/plugins/datatables.js
@@ -0,0 +1,8 @@
+let jquery_datatable = 
+    $("#table1").dataTable({
+        aLengthMenu: [
+            [10, 25, 50, 100, -1],
+            [10, 25, 50, 100, "All"]
+        ],
+        iDisplayLength: 10
+    });
