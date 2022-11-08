cb5ade6ef2c05c05ae3ad643ec9119acbd835c5f
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-11-08 10:41:42 +0700

    Update static files

--- kawn_subscriptions_manager/static/js/plugins/datatables.js
+++ kawn_subscriptions_manager/static/js/plugins/datatables.js
@@ -1,8 +1,27 @@
-let jquery_datatable = 
+let jquery_datatable =
     $("#table1").dataTable({
-        aLengthMenu: [
+        "dom": "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+            "<'row'<'col-sm-12'tr>>" +
+            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+        "buttons": [
+            'excel',
+            'csv',
+            'pdf'
+        ],
+        "lengthMenu": [
             [10, 25, 50, 100, -1],
             [10, 25, 50, 100, "All"]
         ],
-        iDisplayLength: 10
+        "iDisplayLength": 10,
+        "pagingType": 'full_numbers',
+        "language": {
+            'paginate': {
+                'first': '<i class="fa-solid fa-angles-left"></i>',
+                'previous': '<i class="fa-solid fa-angle-left"></i>',
+                'next': '<i class="fa-solid fa-angle-right"></i>',
+                'last': '<i class="fa-solid fa-angles-right"></i>',
+            }
+        },
+        // "responsive": true,
+        // "autoWidth": false,
     });
