e3a8f7f99ba205358f9cfc60334637a3426ec954
Samuel Halomoan Manalu <samman.r007@gmail.com>
2022-11-08 10:55:16 +0700

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Jakarta"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
@@ -219,6 +219,7 @@ TEMPLATES = [
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
                 "kawn_subscriptions_manager.users.context_processors.allauth_settings",
+                "kawn_subscriptions_manager.context_processor.notification",
             ],
         },
     }
--- config/urls.py
+++ config/urls.py
@@ -5,12 +5,18 @@ from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required, permission_required
+from kawn_subscriptions_manager.dashboard.views import SubscriptionLogsCreated, SubscriptionLogsModified
 
 urlpatterns = [
     path(
         "",
-        login_required(TemplateView.as_view(template_name="dashboard/dashboard.html")),
-        name="home",
+        login_required(SubscriptionLogsCreated.as_view()),
+        name="home1",
+    ),
+    path(
+        "",
+        login_required(SubscriptionLogsModified.as_view()),
+        name="home2",
     ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
