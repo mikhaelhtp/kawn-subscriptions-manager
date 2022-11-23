f45e17cabe94e279b899dc4b7a41aad362286e34
Mikhael Hutapea <mhutapea751@gmail.com>
2022-11-23 18:15:52 +0700

    Add Command
    Add command for call API

---
+++ kawn_subscriptions_manager/clients/management/commands/call_api_1.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from kawn_subscriptions_manager.api import (
+    api_1,
+)
+
+class Command(BaseCommand):
+    help = 'Call API Except City and Subscription'
+
+    def handle(self, *args, **kwargs):
+        api_1
