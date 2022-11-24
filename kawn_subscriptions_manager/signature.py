47c615d65660054cdb50102113474b8aa630f3b8
Mikhael Hutapea <mhutapea751@gmail.com>
2022-11-24 10:07:02 +0700

    Update signature.py
    Make env variable for key and secret

--- kawn_subscriptions_manager/signature.py
+++ kawn_subscriptions_manager/signature.py
@@ -2,9 +2,15 @@ import requests
 import datetime
 from httpsig.requests_auth import HTTPSignatureAuth
 from kawn_subscriptions_manager.clients.models import Province
+import environ
 
-KEY_ID = "a206c5980f385ba96ff3f1b5979986c0d11a058e"
-SECRET = "33d1ac21bec6cae44c2d7bedcb521938b9b3d436"
+env = environ.Env()
+environ.Env.read_env()
+
+KEY_ID = env("KEY_ID")
+SECRET = env("SECRET")
+DOMAIN = "indev.kawn.co.id"
+
 e = datetime.datetime.now()
 
 signature_headers = ["(request-target)", "date"]
@@ -15,19 +21,19 @@ auth = HTTPSignatureAuth(
 )
 
 req = requests.get(
-    "https://indev.kawn.co.id/api/v2.1/internal-application/subscription/",
+    "https://"+ DOMAIN +"/api/v2.1/internal-application/subscription/",
     auth=auth,
     headers=headers,
 ).json()
 
 reqoutlet = requests.get(
-    "https://indev.kawn.co.id/api/v2.1/internal-application/outlet/",
+    "https://"+ DOMAIN +"/api/v2.1/internal-application/outlet/",
     auth=auth,
     headers=headers,
 ).json()
 
 prov = requests.get(
-    "https://indev.kawn.co.id/api/v2.1/provinces/",
+    "https://"+ DOMAIN +"/api/v2.1/provinces/",
     auth=auth,
     headers=headers,
 ).json()
@@ -36,20 +42,20 @@ p = Province.objects.all().order_by("id")
 cities = tuple()
 for i in p:
     city = requests.get(
-        "https://indev.kawn.co.id/api/v2.1/provinces/"+ str(i.id) +"/cities/",
+        "https://"+ DOMAIN +"/api/v2.1/provinces/"+ str(i.id) +"/cities/",
         auth=auth,
         headers=headers,
     ).json()
     cities += tuple(city["results"])
 
 account = requests.get(
-    "https://indev.kawn.co.id/api/v2.1/account/",
+    "https://"+ DOMAIN +"/api/v2.1/account/",
     auth=auth,
     headers=headers,
 ).json()
 
 subscription_plan = requests.get(
-    "https://indev.kawn.co.id/api/v2.1/internal-application/subscription-plan/",
+    "https://"+ DOMAIN +"/api/v2.1/internal-application/subscription-plan/",
     auth=auth,
     headers=headers,
 ).json()
