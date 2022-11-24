1477d26313754428fdfefc4db345e0a888c89f51
Agus <agus.wahyudi@tech.indosterling.com>
2022-11-24 12:12:54 +0700

    remove unused asyncio import

--- kawn_subscriptions_manager/subscriptions/models.py
+++ kawn_subscriptions_manager/subscriptions/models.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from django.core.exceptions import ValidationError
-from asyncio.windows_events import NULL
 from email.policy import default
 from django.db import models
 from django.utils.translation import gettext_lazy as _
