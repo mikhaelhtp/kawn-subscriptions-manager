7eddc737e7ce9ca3c4a2f97021af6ad33732a699
Mikhael Hutapea <mhutapea751@gmail.com>
2022-12-14 16:17:08 +0700

    Activate celery
    Activate celery

--- config/celery_app.py
+++ config/celery_app.py
@@ -1,6 +1,9 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+
+from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
@@ -11,7 +14,51 @@ app = Celery("kawn_subscriptions_manager")
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
+app.conf.enable_utc = True
+app.conf.timezone = 'Asia/Jakarta'
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+# Executes every day morning at 6:00 a.m.
+'get_province': {
+    'task': 'kawn_subscriptions_manager.clients.tasks.get_province',
+    'schedule': crontab(hour=6, minute=0),
+}, 
+'get_city': {
+    'task': 'kawn_subscriptions_manager.clients.tasks.get_city',
+    'schedule': crontab(hour=6, minute=1),
+},
+'get_outlet': {
+    'task': 'kawn_subscriptions_manager.clients.tasks.get_outlet',
+    'schedule': crontab(hour=6, minute=0),
+},
+'get_subscription_plan': {
+    'task': 'kawn_subscriptions_manager.subscriptions.tasks.get_subscription_plan',
+    'schedule': crontab(hour=6, minute=0),
+},
+'get_subscription': {
+    'task': 'kawn_subscriptions_manager.subscriptions.tasks.get_subscription',
+    'schedule': crontab(hour=6, minute=1),
+},
+# Executes every day afternoon at 6:00 p.m.
+'post_outlet': {
+    'task': 'kawn_subscriptions_manager.clients.tasks.post_outlet',
+    'schedule': crontab(hour=18, minute=0),
+},
+'post_subscription_plan': {
+    'task': 'kawn_subscriptions_manager.subscriptions.tasks.post_subscription_plan',
+    'schedule': crontab(hour=18, minute=0),
+},
+# Executes every day afternoon at 6:30 p.m.
+'put_subscription_plan': {
+    'task': 'kawn_subscriptions_manager.subscriptions.tasks.put_subscription_plan',
+    'schedule': crontab(hour=18, minute=30),
+},
+'put_subscription': {
+    'task': 'kawn_subscriptions_manager.subscriptions.tasks.put_subscription',
+    'schedule': crontab(hour=18, minute=30),
+},
+}
--- config/settings/base.py
+++ config/settings/base.py
@@ -87,8 +87,8 @@ THIRD_PARTY_APPS = [
     "django_filters",
     "phonenumber_field",
     "view_breadcrumbs",
-    "betterforms"
-    # "django_celery_beat",
+    "betterforms",
+    "django_celery_beat",
 ]
 
 LOCAL_APPS = [
@@ -285,29 +285,29 @@ LOGGING = {
     "root": {"level": "INFO", "handlers": ["console"]},
 }
 
-# # Celery
-# # ------------------------------------------------------------------------------
-# if USE_TZ:
-#     # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-timezone
-#     CELERY_TIMEZONE = TIME_ZONE
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
-# CELERY_BROKER_URL = env("CELERY_BROKER_URL")
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_backend
-# CELERY_RESULT_BACKEND = CELERY_BROKER_URL
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-accept_content
-# CELERY_ACCEPT_CONTENT = ["json"]
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-task_serializer
-# CELERY_TASK_SERIALIZER = "json"
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_serializer
-# CELERY_RESULT_SERIALIZER = "json"
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-time-limit
-# # TODO: set to whatever value is adequate in your circumstances
-# CELERY_TASK_TIME_LIMIT = 5 * 60
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-soft-time-limit
-# # TODO: set to whatever value is adequate in your circumstances
-# CELERY_TASK_SOFT_TIME_LIMIT = 60
-# # https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
-# CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+# Celery
+# ------------------------------------------------------------------------------
+if USE_TZ:
+    # https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-timezone
+    CELERY_TIMEZONE = TIME_ZONE
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-broker_url
+CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_backend
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-accept_content
+CELERY_ACCEPT_CONTENT = ["json"]
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-task_serializer
+CELERY_TASK_SERIALIZER = "json"
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#std:setting-result_serializer
+CELERY_RESULT_SERIALIZER = "json"
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-time-limit
+# TODO: set to whatever value is adequate in your circumstances
+CELERY_TASK_TIME_LIMIT = 5 * 60
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-soft-time-limit
+# TODO: set to whatever value is adequate in your circumstances
+CELERY_TASK_SOFT_TIME_LIMIT = 60
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#beat-scheduler
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 # django-allauth
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", False)
